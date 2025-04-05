2f963c2aa5368a78e31982cc605bd4df085ba910
재형 <kim0js@naver.com>
2025-04-05 21:50:28 +0900

    타입가드, 함수 오버로딩 개념

---
+++ section4/src/chapter3.ts
@@ -0,0 +1,23 @@
+/**
+ * 함수 오버로딩
+ * 하나의 함수를 매개변수의 개수나 타입에 따라 여러가지 버전으로 정의
+ * js x 오로지 타입스크립트에서만
+ */
+
+// 버전들 -> 오버로드 시그니처
+function func(a: number): void;
+function func(a: number, b: number, c: number): void;
+
+// 실제 구현부 -> 구현 시그니처
+function func(a: number, b?: number, c?: number) {
+  if (typeof b === "number" && typeof c === "number") {
+    console.log(a + b + c);
+  } else {
+    console.log(a * 20);
+  }
+}
+
+// func();   -> 오류
+func(1);
+// func(1,2); -> 오류
+func(1, 2, 3);
